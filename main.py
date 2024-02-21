64fca42a0c69a8341f54982b4ea40a60270c85d7
style77 <stylek777@gmail.com>
2024-02-21 15:50:22 +0100

    chore: implement more colors

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ from ecosphere.system import System
 def _init_colors():
     curses.start_color()
     if curses.COLORS >= 256:
+        COLOR_LIGHT_GREEN = curses.COLOR_GREEN
+        COLOR_DARK_GREEN = 22
         COLOR_GRAY = 244
     else:
         # Fallback
         COLOR_GRAY = curses.COLOR_WHITE
+        COLOR_LIGHT_GREEN = curses.COLOR_GREEN
+        COLOR_DARK_GREEN = curses.COLOR_GREEN
 
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLUE)
-    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_YELLOW)
-    curses.init_pair(4, curses.COLOR_BLACK, COLOR_GRAY)
-    curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+    curses.init_pair(3, curses.COLOR_BLACK, COLOR_LIGHT_GREEN)
+    curses.init_pair(4, curses.COLOR_BLACK, COLOR_DARK_GREEN)
+    curses.init_pair(5, curses.COLOR_BLACK, COLOR_GRAY)
+    curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
 
 def setup_stdscr():
