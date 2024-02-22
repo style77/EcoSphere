9011e6664637c0eb3550bfd9f1361ceb68b0aa95
style77 <stylek777@gmail.com>
2024-02-22 04:19:40 +0100

    chore: time tracking to SystemInfo class

--- ecosphere/common/systeminfo.py
+++ ecosphere/common/systeminfo.py
@@ -14,6 +14,9 @@ class SystemInfo(metaclass=SingletonMeta):
         self.height = self.stdscr.getmaxyx()[0]
 
         self.entities = Counter()  # Class determined count of entities
+        self._moved = Counter()  # Class determined count of moved entities this tick
+
+        self._time = 0  # Minutes counter
 
     @staticmethod
     @bus.listener("entity:created")
@@ -35,6 +38,16 @@ class SystemInfo(metaclass=SingletonMeta):
         else:
             sysinfo.entities[entity_name] += 1
 
+    @staticmethod
+    @bus.listener("minute:passed")
+    def minute_passed():
+        """
+        Increment the day counter.
+        """
+        sysinfo = SystemInfo()
+
+        sysinfo._time += 1
+
     def draw(self):
         """
         Draw system info to the screen.
@@ -46,5 +59,11 @@ class SystemInfo(metaclass=SingletonMeta):
             for entity, count in self.entities.items():
                 _temp_vals.append(f"{entity} ({count})")
             text += "Entities: " + ", ".join(_temp_vals)
+
+        if self._time:
+            days = self._time // 1440
+            hours = (self._time % 1440) // 60
+            minutes = self._time % 60
+            text += f" | Time: {days}d {hours}h {minutes}m"
 
         self.stdscr.addstr(self.height - 1, 4, text, curses.COLOR_WHITE)
