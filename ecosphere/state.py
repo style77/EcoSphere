e7e96eb8609265daaa05019e6598e9b63524d775
style77 <stylek777@gmail.com>
2024-02-23 10:57:36 +0100

    chore: __repr__ and __str__ methods to AnimalState class

--- ecosphere/state.py
+++ ecosphere/state.py
@@ -16,6 +16,12 @@ class AnimalState(ABC):
     ) -> None:
         pass
 
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return self.__class__.__name__.replace("State", "")
+
 
 class IdleState(AnimalState):
     """
